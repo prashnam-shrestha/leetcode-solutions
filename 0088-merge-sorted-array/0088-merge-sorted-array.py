6af9d90de717cfbe7ac74b8b1daa90bfc41a7434
Prashnam Shrestha <136892213+prashnam-shrestha@users.noreply.github.com>
2026-01-23 00:08:12 +0545

    Time: 0 ms (100%), Space: 19.4 MB (21.32%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,16 +4,12 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         nums_1 = []
-        nums_2 = []
 
         for i in range(m):
             nums_1.append(nums1[i])
         
         for j in range(n):
-            nums_2.append(nums2[j])
-
-        for k in nums_2:
-            nums_1.append(k)
+            nums_1.append(nums2[j])
 
         nums1[:] = sorted(nums_1)
         
