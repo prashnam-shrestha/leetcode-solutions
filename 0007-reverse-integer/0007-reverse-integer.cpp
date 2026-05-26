149a95c2ee359233c1e63de6d44de9683d30c06b
Prashnam Shrestha <136892213+prashnam-shrestha@users.noreply.github.com>
2026-05-26 13:14:06 +0545

    Time: 5 ms (20.6%), Space: 8.5 MB (53.86%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int reverse(int x) { // TC: O(Log10 X)
+
+        int a = 0;
+        while (x != 0) {
+            // Check 32 bits limit first;
+            if (a > INT32_MAX / 10) {
+                return 0;
+            }
+            else if (a < INT32_MIN / 10) {
+                return 0;
+            }
+            // x % 10 gets the last digit
+            a = a * 10 + x % 10;
+            
+            // x / 10 removes the last digit
+            x = x / 10;
+        }
+        return a;
+    }
+};
